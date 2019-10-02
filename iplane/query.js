32f68da69c91d6fdcd6bd185d630aec0c599636e
Yousef Amar <yousefamar@gmail.com>
2019-10-02 15:50:38 +0100

    Remove iplane interfacing

--- iplane/query.js
+++
@@ -1,8 +0,0 @@
-const xmlrpc = require('xmlrpc');
-
-const client = xmlrpc.createClient({ host: 'iplane.cs.washington.edu', port: 7820, path: '/'})
-
-client.methodCall('iplane.query', [{ src: '46.101.140.219', dst: '216.58.204.46' }], function (error, value) {
-	console.log(error);
-	console.log('Method response for \'anAction\': ' + value);
-})
