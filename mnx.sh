d44348f269087be25ed7306a0f62a9de53d1b7f7
Yousef Amar <yousefamar@gmail.com>
2020-02-09 20:25:22 +0000

    Install git in mnx script

--- mnx.sh
+++ mnx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+# base-64-lts (19.x.x)
+# on edit: scp mnx.sh amar.io:~/www
+# curl https://amar.io/mnx.sh | bash
 
+yes | pkgin install git
 git clone https://github.com/yousefamar/p2p-sim.git
 cd p2p-sim
 npm install
